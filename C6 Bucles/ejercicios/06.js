6420133c71496a568f8d161a5ba3aafab613292e
ignacioRZ <ignale.ruiz@gmail.com>
2024-05-28 22:02:29 -0300

    M6 bucles completado

--- C6 Bucles/ejercicios/06.js
+++ C6 Bucles/ejercicios/06.js
@@ -2,6 +2,8 @@ function tieneTresDigitos(num) {
   // Si el número recibido tiene tres dígitos, retorna true.
   // Caso contrario, retorna false.
   // Tu código:
+  const valorAbsoluto = Math.abs(num);
+  return valorAbsoluto >= 100 && valorAbsoluto <= 999;
 }
 
 module.exports = tieneTresDigitos;
